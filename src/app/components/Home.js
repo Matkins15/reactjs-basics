7370909d85407030f7933235bca6606c40d7ee91
Matthew Atkins <matkinsdesigns@gmail.com>
2018-04-27 15:45:39 -0400

    targeting children props in component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react'
 
 export class Home extends Component {
   render () {
+    console.log(this.props)
     return (
       <div>
-        <p>In an new Component</p>
+        <h1>{this.props.user.name}</h1>
+        <h4>{this.props.user.title}</h4>
+        <h6>{this.props.user.location}</h6>
+        <div>
+          {this.props.children}
+        </div>
+
       </div>
     )
   }
 }
 
-// export default Home
+// Home.propTypes = {
+//   name: React.PropTypes.string,
+//   title: React.PropTypes.string,
+//   location: React.PropTypes.string,
+//   age: React.PropTypes.number
+// }
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,12 @@ import { Home } from './components/Home'
 
 class App extends Component {
   render () {
+    let user = {
+      name: 'Matthew Atkins',
+      title: 'Front-End-Developer',
+      location: 'Atlanta, GA',
+      age: 30
+    }
     return (
       <div className='container'>
         <div className='row'>
@@ -14,7 +20,9 @@ class App extends Component {
         </div>
         <div className='row'>
           <div className='col-xs-10 col-xs-offset-1'>
-            <Home />
+            <Home user={user}>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </Home>
           </div>
         </div>
       </div>
